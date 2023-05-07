bee9c8684c0ac7d637bcb0bc55d30a2792ba5adc
Upal Bhattacharya <upal.bhattacharya@gmail.com>
2023-05-08 00:42:12 +0530

    Updated locker timeout

--- .config/awesome/locker.sh
+++ .config/awesome/locker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 exec xautolock -detectsleep \
-    -time 3 -locker "betterlockscreen -l" \
+    -time 5 -locker "betterlockscreen -l" \
     -notify 30 \
     -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'"
 
